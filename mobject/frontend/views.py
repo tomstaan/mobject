58a62946ef4f2317b5a7cb63a7f6ae35a266ca47
Tomasz Stankiewicz <36516273+TomSCoding@users.noreply.github.com>
2019-05-23 23:24:12 +0100

    React integration

--- mobject/frontend/views.py
+++ mobject/frontend/views.py
@@ -1,3 +1,4 @@
 from django.shortcuts import render
 
-# Create your views here.
+def input(request):
+    return render(request, 'frontend/index.html')
--- mobject/mobject/urls.py
+++ mobject/mobject/urls.py
@@ -3,6 +3,7 @@ from django.urls import path, include
 
 urlpatterns = [
     #path('admin/', admin.site.urls),
+    path('', include('frontend.urls')),
     path('', include('objects.urls')),
 
 ]
